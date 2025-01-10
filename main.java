14ebf5a2b88b3cf022f29afce3c8dd133636fc56
potemkinreal <ej7313@pleasantonusd.net>
2025-01-10 14:48:09 -0800

    made the program
    should work

--- main.java
+++ main.java
@@ -1,1 +1,16 @@
-
+public class Main
+{
+  public static void main(String [] args){
+    int total = 0;
+    for(int i = 1; i<334; i++){
+      total += i*3;
+    }
+    for(int i =1; i<201; i++){
+      if((i*5)%3 != 0){
+        total += i*5; 
+      }
+    }
+    System.out.print(total);
+  }
+}
+  
